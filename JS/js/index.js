e54ac835579aa6603d7a2a8428704ec5a3b5a10d
Kecvin-12-u <1719110155@uteculancingo.edu.mx>
2022-12-13 22:28:07 -0600

    valores boleanos

--- JS/js/index.js
+++ JS/js/index.js
@@ -6,5 +6,25 @@
 console.log("\n-------Variables-----------\n");
 var numero_1 = 1;
 let numero_2 = 2;
-const numero_3 = 3;
-console.log("Numero 1 = " + numero_1 + ", Numero 2 = " + numero_2);
+const numero_3 = 03;
+console.log("Número 1 = " + numero_1 + ", Número 2 = " + numero_2 + ", Número 3 = " + numero_3);
+
+/* cadenas de caracteres 'Strings' pueden definirse con comillas dobles ""  o con comillas simpres ''
+    o en otros casos con comillas invertidas ``
+
+    meter valores en una cadena es con ${variable} pero solo con asentos invertidos
+ */
+
+console.log('\n=====Cadenas de texto=====\n');
+var cadena1 = "Frase 1";
+let cadena2 = 'frase 2';
+const cadena3 = `Esto es una cadena concatenada ${numero_1}`;
+console.log(cadena1 + "\n" + cadena2 + "\n" + cadena3);
+
+/* los condicionales se pueden usar como valores <> = != == === 
+    y cada una tiene una funcionalidad de comparación de elementos*/
+
+const text = "====Comparacion de variables===="
+console.log(text)
+console.log(numero_1 == numero_2);
+
