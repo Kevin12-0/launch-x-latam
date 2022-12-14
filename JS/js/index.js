3e78cec3a3aa863719bc3348db6e93e554013b51
Kecvin-12-u <1719110155@uteculancingo.edu.mx>
2022-12-13 22:55:05 -0600

    operadores logicos

--- JS/js/index.js
+++ JS/js/index.js
@@ -6,7 +6,7 @@
 console.log("\n-------Variables-----------\n");
 var numero_1 = 1;
 let numero_2 = 2;
-const numero_3 = 03;
+const numero_3 =3;
 console.log("Número 1 = " + numero_1 + ", Número 2 = " + numero_2 + ", Número 3 = " + numero_3);
 
 /* cadenas de caracteres 'Strings' pueden definirse con comillas dobles ""  o con comillas simpres ''
@@ -34,11 +34,14 @@ console.log(numero_1 != numero_2);
 /* Operadores logicos se usan cuando se nesesitan comparar mas de un condional
 AND > && nesesita que todos sus valores sean true para que regrese un true en una salida
 OR >> || necesita que solo uno de sus valores sea true para que regrese un resultado
+en cuestion de jerarquia el OR tienen mas orden jeracquico que el AND
 */
 
 let message = 'Operador logico AND > &&';
 console.log(message);
 console.log(true && true);
+console.log(numero_1 < numero_2 && numero_2 !== numero_3);
 let message_2 = 'Operador logico OR > ||';
 console.log(message_2);
 console.log(false || false);
+console.log(numero_2 === numero_3 || numero_2 < numero_3);
