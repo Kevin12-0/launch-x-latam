6d6f812085bd6bc415fa7c649c5909b41b29a46a
Kecvin-12-u <1719110155@uteculancingo.edu.mx>
2022-12-13 21:57:52 -0600

    variables

---
+++ JS/js/index.js
@@ -0,0 +1,10 @@
+/* las variables se pueden declarar usando la palabra reservada 'var' o 'let' let es una palabra reservada
+    mas moderna y además que es mejor visto para las practicas de programación mas modernas 
+    'const' es una palabra reservada que se usa para valores constantes osea que no vam a cambiar
+ */
+
+console.log("\n-------Variables-----------\n");
+var numero_1 = 1;
+let numero_2 = 2;
+const numero_3 = 3;
+console.log("Numero 1 = " + numero_1 + ", Numero 2 = " + numero_2);
