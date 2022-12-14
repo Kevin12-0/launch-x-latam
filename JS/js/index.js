d37faa3bb841b9afa3d8b999807ff25d9c421516
Kecvin-12-u <1719110155@uteculancingo.edu.mx>
2022-12-13 22:39:21 -0600

    operadores logicos

--- JS/js/index.js
+++ JS/js/index.js
@@ -22,9 +22,23 @@ const cadena3 = `Esto es una cadena concatenada ${numero_1}`;
 console.log(cadena1 + "\n" + cadena2 + "\n" + cadena3);
 
 /* los condicionales se pueden usar como valores <> = != == === 
-    y cada una tiene una funcionalidad de comparación de elementos*/
+    y cada una tiene una funcionalidad de comparación de elementos
+    retorna 'true' o 'false'
+    */
 
-const text = "====Comparacion de variables===="
+const text = "====Comparacion de variables (boolean)===="
 console.log(text)
 console.log(numero_1 == numero_2);
+console.log(numero_1 != numero_2);
 
+/* Operadores logicos se usan cuando se nesesitan comparar mas de un condional
+AND > && nesesita que todos sus valores sean true para que regrese un true en una salida
+OR >> || necesita que solo uno de sus valores sea true para que regrese un resultado
+*/
+
+let message = 'Operador logico AND > &&';
+console.log(message);
+console.log(true && true);
+let message_2 = 'Operador logico OR > ||';
+console.log(message_2);
+console.log(false || false);
