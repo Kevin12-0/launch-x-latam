f9639dd6f1a5685668f4808adbb0ddc82052885e
Kecvin-12-u <65631970+Kevin12-0@users.noreply.github.com>
2022-12-15 16:23:08 -0600

    funciones en navegadores sin consumo de API´s

---
+++ JS/js/navegadores.js
@@ -0,0 +1,19 @@
+/* encontrar elementos en html */
+
+const titulo = document.getElementById("titulo1");
+const parrafo = document.getElementsByTagName('p');
+const links = document.getElementsByClassName('links');
+/* 
+    Otra opcióon para encontrar elementos en HTML
+const titulo2 = document.querySelector("[id=titulo]"); */
+
+/* cambiar valores de html */
+titulo.innerHTML = "Titulo cambiado con JS";
+document.getElementById("demo").innerHTML = "The text in the first paragraph (index 0) is: " + parrafo[0].innerHTML;
+links[0].href = "https://www.youtube.com/";
+links[0].target = "_blank";
+
+function cambiartexto(obj){
+    obj.innerHTML ="Texto clickeado"
+}
+
